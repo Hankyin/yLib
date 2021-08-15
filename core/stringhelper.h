1699f0ff7a5a4955e07f76f02a164cc8c10a0b60
yin <loufand@163.com>
2021-08-15 19:09:31 +0800

    调整目录结构2

--- core/stringhelper.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-//实现了一些string的常用操作。
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> string_split(const std::string &_str, char ch); //根据字符分割字符串
-std::string string_trim(const std::string &str);                         //去除字符串前后空白，回车，换行等
